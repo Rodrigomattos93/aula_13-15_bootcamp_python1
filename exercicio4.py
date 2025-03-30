b9748444f0f8beb3d6e7eb03e6c8cc25a430005e
Rodrigo Mattos <rodrigo.mattos.rocha@poli.ufrj.br>
2025-03-29 21:00:29 -0300

    adicionando enunciados das questoes 4 e 5

---
+++ exercicio4.py
@@ -0,0 +1,7 @@
+#Questão 4 - Herança e Polimorfismo (Difícil)
+#Crie uma classe Funcionario com um método calcular_salario() que retorna um salário base. 
+# Depois, crie duas classes Gerente e Desenvolvedor que herdam de Funcionario e sobrescrevem 
+# o método calcular_salario(), considerando que:
+#O Gerente recebe o dobro do salário base.
+#O Desenvolvedor recebe 50% a mais do salário base.
+#Crie instâncias de ambas as classes e exiba os salários calculados.
---
+++ exercicio5.py
@@ -0,0 +1,10 @@
+#Questão 5 - Desafio Final: Recursividade com Classes (Muito Difícil)
+#Crie uma classe Fibonacci com um método recursivo calcular(n) que retorna o n-ésimo 
+# termo da sequência de Fibonacci.
+#Exemplo:
+#python
+#Copiar
+#Editar
+#fib = Fibonacci()
+#print(fib.calcular(6))  # Saída: 8 (0, 1, 1, 2, 3, 5, 8)
+
